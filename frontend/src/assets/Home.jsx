ebb3f1b5623f7793f04770801d77138df5f8a11c
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2026-02-07 11:43:45 +0530

    Connect to localhost

---
+++ frontend/src/assets/Home.jsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+
+function Home() {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8081/')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+    }, [])
+
+  return (
+    <div>
+      <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID </th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((student, index) => {
+                    return <tr key={index}>
+                        <td>{student.ID}</td>
+                        <td>{student.Name}</td>
+                        <td>{student.Email}</td>
+                        </tr>
+                })}       
+            </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}   
+
+export default Home
