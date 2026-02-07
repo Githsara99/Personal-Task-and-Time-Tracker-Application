1f6dc683ac0fa053bfde5e569df8c36901bddd5c
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2026-02-07 10:32:52 +0530

    created server.js file

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql'
+
+const app = express();
+app.use(cors());
+
+app.listen(8081, ()=>{
+    console.log("Server is running on port 8081");
+});
