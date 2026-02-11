e9557383f2612f05a7404f353c783ced386a57b8
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2026-02-11 12:22:28 +0530

    Alignment changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,3 @@
-// import React from "react"
-// import {BrowserRouter, Routes, Route} from "react-router-dom"
-// import Home from "./Components/Home"
-// import Create from "./Components/Create"
-// import Read from "./Components/Read"
-// import Edit from "./Components/Edit"
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/create' element={<Create />} />
-//         <Route path='/read/:id' element={<Read />} />
-//         <Route path='/edit/:id' element={<Edit />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
